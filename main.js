bf0fbfd17a3d288836cadb1b25aba70aed206cb4
danejackins <dane.jackins@gmail.com>
2018-02-26 20:11:14 -0700

    third commit

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
 
         else if (userScore > compScore) {
             loses++;
-            $("#loses").text(loses)
+            $("#losses").text(loses)
         }
     }
 
